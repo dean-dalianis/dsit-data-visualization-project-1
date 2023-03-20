22c3f6da7b86b51f26d7a744d11def4602919241
Dean Dalianis <dean.dalianis@cern.ch>
2023-03-20 15:44:12 +0200

    Updated script and added result pie-charts and added FR pie chart

--- top5GenresForTop5TravelDestinations.py
+++ top5GenresForTop5TravelDestinations.py
@@ -1,23 +1,21 @@
-import requests
-import spotipy
-import matplotlib.pyplot as plt
 import collections
-import folium
+import matplotlib.pyplot as plt
+import spotipy
 
-destinations = ['FR']#, 'ES', 'IT', 'USA', 'JP']
+destinations = ['FR', 'ES', 'IT', 'US', 'JP']
 
 # Step 2: Get data on most listened-to music genres in those destinations
-spotify = spotipy.Spotify(auth='BQDAMSKzxzmUL6FK3sxkpkOzl4UmYPyotuJbXbEyHJwDvT4mJAl94QE1-kmbbaB91iRDnH6Mst47extunCOxB2VFyz_W7rAqhAcAkE8JMKLJtXfQ9zf3t1829Kol2jvqUZnSdaKuciXY0LDM2sLDK41v7ktLgkUsmU5IDs-Kk0TsGQxgNgnMB6fQMqow-1c')
+spotify = spotipy.Spotify(
+    auth='BQDAMSKzxzmUL6FK3sxkpkOzl4UmYPyotuJbXbEyHJwDvT4mJAl94QE1-kmbbaB91iRDnH6Mst47extunCOxB2VFyz_W7rAqhAcAkE8JMKLJtXfQ9zf3t1829Kol2jvqUZnSdaKuciXY0LDM2sLDK41v7ktLgkUsmU5IDs-Kk0TsGQxgNgnMB6fQMqow-1c')
 genres_data = []
 for destination in destinations:
     genres = []
     for playlist in spotify.category_playlists("toplists", country=destination)['playlists']['items']:
         tracks = spotify.playlist_items(playlist['id'], fields='items(track(name,artists))')
         for track in tracks['items']:
-            if track['track']:
-                if track['track']['artists']:
-                    for artist in track['track']['artists']:
-                        genres += spotify.artist(artist['id'])['genres']
+            if track.get('track') and track['track'].get('artists'):
+                for artist in track['track']['artists']:
+                    genres += spotify.artist(artist['id'])['genres']
     # Count the frequency of each genre
     genre_count = collections.Counter(genres)
     # Get the top 5 most common genres
@@ -26,24 +24,17 @@ for destination in destinations:
     print(top_genres)
     print(genres_data)
 
-# Step 3: Create a choropleth map visualization
-m = folium.Map(location=[48.864716, 2.349014], zoom_start=2)
+# Step 3: Create a pie chart for each country
 for data in genres_data:
     country_name = data['country']
     top_genres = data['top_genres']
     if not top_genres:
         continue
-    folium.Choropleth(
-        geo_data='https://raw.githubusercontent.com/python-visualization/folium/master/examples/data',
-        name=country_name,
-        data=top_genres,
-        columns=[('genre', 'count')],
-        key_on='feature.properties.name',
-        fill_color='YlOrRd',
-        fill_opacity=0.7,
-        line_opacity=0.2,
-        legend_name='Top 5 Music Genres'
-    ).add_to(m)
-
-# Save the map as an HTML file
-m.save('music_genre_map.html')
+    labels, sizes = zip(*top_genres)
+    fig, ax = plt.subplots()
+    ax.set_title(country_name)
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')
+    # Save the pie chart as an SVG file
+    fig.savefig(f'{country_name}_pie_chart.svg', format='svg')
+    plt.close(fig)
